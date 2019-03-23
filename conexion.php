18ddd642cc48b656882afa53b7701e7cd5772530
falfonsoPrg <38541967+falfonsoPrg@users.noreply.github.com>
2019-03-22 21:58:47 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $bd = "cazuca";
-//Variable que contiene la conexion
+//Variable que contiene la conexion hecho desde git
 $conn = mysqli_connect($servername, $username, $password,$bd);
 
 ?> 
