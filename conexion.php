f6ebd03e6494bebec651e514d28bd24f2662d03e
Fabian Alfonso <falfonso@unbosque.edu.co>
2019-03-22 21:58:01 -0500

    Documentando

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $bd = "cazuca";
-
+//Variable que contiene la conexion
 $conn = mysqli_connect($servername, $username, $password,$bd);
 
 ?> 
